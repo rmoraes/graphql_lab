256d470daf639f7a3140529a614bf49c4e9e88e5
rmoraes <ramoraess@gmail.com>
2024-12-29 15:04:23 -0300

    feat: update user

---
+++ app/graphql/mutations/update_user.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Mutation to update an existing user.
+  # Accepts user attributes, including optional nested attributes for the address, through the `input` argument.
+  # Returns the updated user and a list of errors (if any).
+  class UpdateUser < BaseMutation
+    argument :id, ID, required: true, description: 'The ID of the user to update'
+    argument :name, String, required: false, description: 'The new name of the user'
+    argument :email, String, required: false, description: 'The new email of the user'
+    argument :address, Types::Input::AddressInputType, required: false, description: 'The new address of the user'
+
+    field :user, Types::Models::UserType, null: true, description: 'The updated user'
+    field :errors, [String], null: false, description: 'A list of errors encountered during user update'
+
+    def resolve(id:, name: nil, email: nil, address: nil)
+      user = User.find_by(id: id)
+
+      raise GraphQL::ExecutionError, 'User not found' unless user
+
+      attributes = { name: name, email: email }.compact
+      address_hash = address.to_h if address
+      attributes[:address_attributes] = address_hash if address_hash
+
+      if user.update(attributes)
+        { user: user }
+      else
+        { user: nil, errors: user.errors.full_messages }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,17 +2,8 @@
 
 module Types
   # The MutationType class defines the root mutation type for the GraphQL schema.
-  # Mutations represent actions that can change the server-side data, such as creating,
-  # updating, or deleting resources.
-  #
-  # This class is generated as a starting point for adding mutations to the application.
-  # Each field in this class represents a mutation that the client can execute.
-  #
-  # Example:
-  # - `field :create_user` might represent a mutation to create a new user in the database.
-  #
-  # Currently, this class includes a placeholder mutation field `test_field` as an example.
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
+    field :update_user, mutation: Mutations::UpdateUser
   end
 end
